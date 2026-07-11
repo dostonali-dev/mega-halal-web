db171b57592e5824d107afd9cbea2844ee322e02
Doston Ali <dostonalibek@gmail.com>
2026-07-12 05:25:03 +0900

    telegram order

---
+++ app/api/order/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const BOT_TOKEN = "8798311944:AAHUBgMJ4OrKiy8qMUwx9bQFSNRJ-dRBCjg";
+  const CHAT_ID = "90771259";
+
+  const text = `
+🛒 YANGI BUYURTMA
+
+${body.order}
+
+💰 Jami: ${body.total}₩
+`;
+
+  await fetch(
+    https://api.telegram.org/bot${BOT_TOKEN}/sendMessage,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text,
+      }),
+    }
+  );
+
+  return NextResponse.json({ success: true });
+}
